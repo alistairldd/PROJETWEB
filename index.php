0175032418583099bdb050a6e8c68d7ef168546e
alistairldd <150132411+alistairldd@users.noreply.github.com>
2026-03-12 10:41:21 +0100

    modif vue connexion

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <html>
     <head>
         <title>Mon site web</title>
@@ -35,34 +37,52 @@
             <h1>GROS TITRE DE LA LUNE</h1>
         </div>
 
+        <div class = "Deconnect" style =   "position: absolute; top: 25px; right: 10px;">
+            <?php if ($correct === true): ?>
+                <button id="deconnect" type="button" onclick="deconnect()">Déconnexion</button>
+            <?php endif; ?>
+        </div>
 
+        
         <div class = "Formulaire">
-            <form method="get" action= "index.php">
-                <label for="name">Nom :</label><br>
-                <input type="text" id="name" name="name"><br>
-                <label for="mdp">Mot de Passe :</label><br>
-                <input type="password" id="mdp" name="mdp"><br>
-
-                <?php if ($correct === true):
+            <?php 
+                if ($correct === true):
                     echo "<p>Connexion réussie !</p>";
-                    elseif ($correct === false): 
+                else:
+
+                    if ($correct === false):
                         echo "<p>Identifiants invalides.</p>";
-                endif; ?>
+                    endif;
+                    ?>
 
-                <input type="submit" value="Se connecter">
-            </form> <br> 
-            <p> Ou bien </p>
-            <br>
-            <button id ="boutonform" type="button" onclick="demanderVoyage()">Demande de voyage</button>
+                    <form method="get" action= "index.php">
+                    <label for="name">Nom :</label><br>
+                    <input type="text" id="name" name="name"><br>
+                    <label for="mdp">Mot de Passe :</label><br>
+                    <input type="password" id="mdp" name="mdp"><br>
 
-            <div id = "demandeVoyage" style="display:none;">
-            <form method="post" action="index.php">
-                <label for="mail">Adresse mail :</label><br>
-                <input type="text" id="mail" name="mail"><br>
-                <label for="date">Date :</label><br>
-                <input type="date" id="date" name="date"><br>
-                <input type="submit" value="Demander">
-            </form>
+                    <?php
+                    if ($correct === 'inconnu'): 
+                            echo "<input type='submit' value='Se connecter'>";
+                    endif; 
+                    ?>
+
+                    </form> <br> 
+                    <p> Ou bien </p>
+                    <br>
+                    <button id ="boutonform" type="button" onclick="demanderVoyage()">Demande de voyage</button>
+
+                    <div id = "demandeVoyage" style="display:none;">
+                    <form method="post" action="index.php">
+                        <label for="mail">Adresse mail :</label><br>
+                        <input type="text" id="mail" name="mail"><br>
+                        <label for="dateDeb">Date de début :</label><br>
+                        <input type="date" id="dateDeb" name="dateDeb"><br>
+                        <label for="dateFin">Date de fin :</label><br>
+                        <input type="date" id="dateFin" name="dateFin"><br>
+                        <input type="submit" value="Demander">
+                    </form>
+                <?php endif; ?>
         </div>
 
         </div>
--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function demanderVoyage() {
     $(`#demandeVoyage`).show();
     $(`#boutonform`).hide();
 }
+
+
+function deconnect() {
+    $('#name').val('');
+    $('#mdp').val('');
+    $('#deconnect').hide();
+    window.location.href = "index.php";
+}
